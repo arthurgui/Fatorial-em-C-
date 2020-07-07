4e47b8ac4e8d112a778850b589ca5c025f61342b
Escola de Computação Solidária - UNIPÊ <ecs@microsoft.unipe.br>
2020-07-07 17:58:44 -0300

    soma fatorial

--- BRF1/BRF1/SomaFatorial.cs
+++ BRF1/BRF1/SomaFatorial.cs
@@ -6,9 +6,24 @@ using System.Threading.Tasks;
 
 namespace BRF1
 {
+ 
     class SomaFatorial
     {
-          static void Main(string[] args)
+        static long SomaDigitos(int numero)
+        {
+            int somaFact = 0;
+
+            while (numero != 0)
+            {
+                somaFact += numero % 10;
+                numero = numero / 10;
+            }
+
+            return somaFact;
+        }
+
+        }
+        static void Main(string[] args)
         {
             int i, numero, fact;
             Console.WriteLine("Digite um numero");
@@ -18,8 +33,11 @@ namespace BRF1
             {
                 fact = fact * i;
             }
+            
             Console.WriteLine("\n o fatorial do numero é : "+fact);
             Console.ReadLine();
+            int somaFact = 0;
+            Console.WriteLine("\n a soma do fatorial é:" + somaFact);
 
         }
     }
